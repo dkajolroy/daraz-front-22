a4418623b7e097ebb1367fe0bfc88bf165430c7c
Kajol Roy <shrikajol@gmail.com>
2022-08-01 23:06:09 +0600

    issue fix Router Dom

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import { BrowserRouter, Router, Route } from 'react-router-dom';
+
 import Home from './Pages/Home';
 import ViewProduct from './Pages/ViewProduct';
 import Profile from './Pages/Profile';
 import Cart from './Pages/Cart';
 import Empty from './Pages/Empty';
 import Sign from './Pages/Sign';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <BrowserRouter >
-      <Router>
-        <Route path="/" component={Home} />
-        <Route path="/product/:productId" component={ViewProduct} />
-        <Route path="/profile" component={Profile} />
-        <Route path="/cart" component={Cart} />
-        <Route path="/auth" component={Sign} />
-        <Route path="/*" component={Empty} />
-      </Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:productId" element={<ViewProduct />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/auth" element={<Sign />} />
+        <Route path="/*" element={<Empty />} />
+      </Routes>
     </BrowserRouter>
   );
 }
